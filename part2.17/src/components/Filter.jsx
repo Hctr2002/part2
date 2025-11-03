513702c5d8e5b1d302d667b388ec8ec31831377d
Héctor <hectorrobledo2332@gmail.com>
2025-11-03 13:51:33 -0300

    rearranging

--- part2.17/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filter, handleFilterChange }) => {
-  return (
-    <div>
-      filter show whit <input value={filter} onChange={handleFilterChange} />
-    </div>
-  )
-}
-
-export default Filter
